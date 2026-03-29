d64be7a2035b602f0909e1b726c1cdedbf5ef583
wheatus80 <mwheatley3000@gmail.com>
2026-03-29 09:07:25 +0100

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -329,8 +329,6 @@ class MainWindow(QWidget):
             )
             self.sliders.append(slider)
             self.slider_labels.append(slider_label)
-            slider.raise_()
-            slider_label.raise_()
 
         # Create capital city buttons
         i = 0
